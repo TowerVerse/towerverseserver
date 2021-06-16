22669752c441abf4c2d694ca9b30189572163453
Shadofer <shadowrlrs@gmail.com>
2021-06-16 10:29:45 +0300

    Update circleci config.

--- setup.py
+++ setup.py
@@ -2,16 +2,16 @@ from setuptools import setup
 
 setup(
     name = 'opendogeserver',
-    version = '0.1',
+    version = '0.2a1',
     description = 'The server of opendoge, only used for development purposes.',
-    author = 'Shadofer#7312',
+    author = 'Shadofer#0001',
     author_email = 'shadowrlrs@gmail.com',
     python_requires = '>=3.8.5',
     url = 'https://github.com/OpenDoge/opendogeserver',
     packages = ['opendogeserver'],
-    install_requires = ['websockets==9.1', 'email_validator==1.1.3', 'bcrypt==3.2.0'],
+    install_requires = ['websockets==9.1', 'email_validator==1.1.3', 'bcrypt==3.2.0', 'pymongo==3.11.4'],
     extras_require = {'tests':
                         ['pytest==6.2.4', 'pytest-asyncio==0.15.1', 'pytest-ordering==0.6']
                     },
-    license = 'MIT'
+    license = 'GPL-3'
 )
