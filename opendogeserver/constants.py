ef438b2aebb0c17bf6a68deabbcecd5c7e4d1b47
Reuben Tier <reuben.tier@icloud.com>
2021-06-18 10:48:22 +0100

    Add missing file from last commit

--- opendogeserver/constants.py
+++ opendogeserver/constants.py
@@ -1,16 +1,13 @@
-from sys import argv
-from uuid import uuid4
-
 from pymongo.database import Database
 from pymongo import MongoClient
-
-IS_LOCAL = '--local' in argv
+from sys import argv
 
 mongo_project_name = 'opendoge'
 mongo_database_name = 'opendoge-db'
 mongo_client_extra_args = 'retryWrites=true&w=majority'
 mdbclient: MongoClient = None
-mdb: Database = None
+
+IS_LOCAL = '--local' in argv
 
 """ Max requests until IP ratelimits are cleared. """
 IP_RATELIMIT_MAX = 10
